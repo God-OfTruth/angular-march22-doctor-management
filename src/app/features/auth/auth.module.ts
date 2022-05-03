86e6deb55a4453a11e568111f581be8d0320ea3c
Ritesh Kumar <ritesh.kumar@kloudspot.com>
2022-05-03 17:17:38 +0530

    removed the bug in login page

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -9,6 +9,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
@@ -30,7 +31,8 @@ import { UsernameDisplayComponent } from './components/username-display/username
     MatToolbarModule,
     MatIconModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatInputModule
   ],
   exports: [
     UsernameDisplayComponent
